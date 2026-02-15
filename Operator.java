017205cb788f4c6a9a3841d27eb5e636d0024a5b
sharmilaravi2103-cmyk <sharmilaravi2103@gmail.com>
2026-02-15 22:39:37 +0530

    first commit

---
+++ Operator.java
@@ -0,0 +1,16 @@
+public class Operator{
+public static void main(String[]args)
+{
+int a = 6;
+int b = 4;
+int c = 10;
+int answer1 = a*b+c;
+int answer2 = a*(b+c);
+int answer3 = a/b-c;
+int answer4 = a-c/b;
+System.out.println(answer1);
+System.out.println(answer2);
+System.out.println(answer3);
+System.out.println(answer4);
+}
+}
