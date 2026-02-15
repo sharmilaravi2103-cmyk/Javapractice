47638189f91fb379299dc5201b0a271d24a65a18
sharmilaravi2103-cmyk <sharmilaravi2103@gmail.com>
2026-02-15 22:34:22 +0530

    first commit

---
+++ Greatestnumber.java
@@ -0,0 +1,26 @@
+public class Greatestnumber{
+public static void main(String[] args) 
+{
+int a = 90;
+int b = 80;
+int c = 85;
+
+if (a>b){
+	if (a>c){
+		System.out.println("The Greatest Number is : A");
+	}
+	else{
+		System.out.println("The Greatest Number is : C");
+	}
+}
+else{
+	if(b>c){
+		System.out.println("The Greatest Number is : B");
+	}
+	else{
+		System.out.println("The Greatest Number is : C");
+	}
+}
+}
+}
+      
