39aa8c703c384fbb0f0c0da2e96290bc7be2acfd
sharmilaravi2103-cmyk <sharmilaravi2103@gmail.com>
2026-02-15 22:35:10 +0530

    first commit

---
+++ ArrayConcepts/reverse.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class reverse{
+	public static void main(String[]args){
+		Scanner obj = new Scanner(System.in);
+		int a[] = new int[5];
+		
+		for(int i=0; i<a.length; i++){
+			System.out.println("Enter the value for an index : " +i);
+			a[i] = obj.nextInt();
+		}
+		
+		for(int j=a.length-1; j>=0 ; j--){
+			System.out.println(a[j]);
+		}
+	}
+}
