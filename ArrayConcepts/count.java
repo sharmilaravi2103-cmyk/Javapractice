dc9c191caacd48fee0f964bbdd01beff50c438fb
sharmilaravi2103-cmyk <sharmilaravi2103@gmail.com>
2026-02-15 22:22:08 +0530

    first commit

---
+++ ArrayConcepts/count.java
@@ -0,0 +1,11 @@
+
+public class count{
+	public static void main(String[]args){
+		int a[] ={5,10,15,20,25};
+		int count = 0;
+		for(int i =0 ;i<a.length;i++){
+		count++;
+		}
+		System.out.println(count);
+	}
+}
